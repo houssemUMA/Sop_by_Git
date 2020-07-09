dbababf08ef8e8829093b45d0bbe8f650572d6bb
user <he.bensmida@gmail.com>
2020-07-09 23:58:43 +0200

    we calculate the profit and the distance of initial Solution.

--- Sop_by_git/src/com/sop/git/Git.java
+++ Sop_by_git/src/com/sop/git/Git.java
@@ -10,6 +10,9 @@ import java.util.Scanner;
 public class Git {
 	private int set_number;
 	private int pointsNumber = 52;
+	private double tMax = 1616;
+//	private double distanceTraveled;
+//	private int totalProfit;
 	private int[] set_profit;
 	public int[] points_profit = new int[pointsNumber];
 	public int[] points_cluster = new int[pointsNumber];
@@ -18,6 +21,9 @@ public class Git {
 	public double[][] points = new double[pointsNumber][2];
 	public double m_Distance[][] = new double[pointsNumber][pointsNumber];
 	List<Integer> randomSet;
+	List<Integer> randomPath = new ArrayList<Integer>();
+	List<Integer> firstPath = new ArrayList<Integer>();
+
 
 	public static void main(String[] args) {
 		Git test = new Git();
@@ -29,6 +35,7 @@ public class Git {
 		fillDistanceMatrix();
 		prepare_data();
 		generateRandomPath();
+		create_initial_solution();
 	}
 
 	public void read_data() {
@@ -82,15 +89,20 @@ public class Git {
 	}
 
 	public void prepare_data() {
-		// fill_set_profit is : each cluster has its profit
+		// fill_set_profit is : each cluster has its profit ****set_profit
 		fill_set_profit();
-		// fill_set_cluster is : each cluster has its vertices
+		// fill_set_cluster is : each cluster has its vertices****set_cluster
 		fill_set_cluster();
-		// the index of the array is the id point and the value of each case is the
-		// profit
+		/*
+		 *  the index of the array is the id point and the value of each case is the
+		 *  profit****points_profit
+		 */
 		fill_points_profit();
-		// the index of the array is the id point and the value of each case is the
-		// cluster id
+		/*
+		 *  the index of the array is the id point and the value of each case is the
+		 *  cluster id****points_cluster
+		 */
+		
 		fill_points_cluster();
 	}
 
@@ -152,5 +164,87 @@ public class Git {
 			randomSet.add(i);
 		}
 	}
+	
+	public void create_initial_solution() {
+		generateInitialSolution();
+		generate_first_path(randomPath, firstPath);
+
+	}
+	
+	public void generateInitialSolution() {
+		verify_clear_list(randomPath);
+		randomPath.add(0);
+		int index = 0;
+		for (int i = 1; i < set_number; i++) {
+			index = findNearstPoint(index, randomSet.get(i));
+			randomPath.add(index);
+		}
+
+		System.out.println(randomPath);
+
+	}
+	
+	private void verify_clear_list(List<Integer> list) {
+		if (list.size() > 0) {
+			list.clear();
+		}
+	}
+
+	private int findNearstPoint(int pointA, int clusterB) {
+		double distance;
+//		int currentProfit;
+		int index = set_cluster[clusterB][0];
+		distance = m_Distance[pointA][index];
+		for (int i = 1; i < set_cluster[clusterB].length; i++) {
+			if (distance > m_Distance[pointA][set_cluster[clusterB][i]]) {
+				distance = m_Distance[pointA][set_cluster[clusterB][i]];
+				index = set_cluster[clusterB][i];
+			}
+		}
+//		// We add the new distance to the total distance
+//		distanceTraveled = distanceTraveled + distance;
+
+//		currentProfit = set_profit[clusterB];
+//		// we add the new profit to the total profit
+//		totalProfit = totalProfit + currentProfit;
+		return index;
+	}
+	
+	public void generate_first_path(List<Integer> long_path, List<Integer> short_path) {
+		verify_clear_list(short_path);
+		double local_distance = 0;
+		int i = 0;
+		while (local_distance < tMax) {
+
+			short_path.add(long_path.get(i));
+			local_distance = local_distance + m_Distance[long_path.get(i)][long_path.get(i + 1)];
+			i++;
+		}
+		caluculate_path_distance(short_path);
+		caluculate_path_profit(short_path);
+		System.out.println(short_path);
+	}
+
+	
+	public double caluculate_path_distance(List<Integer> path) {
+		double distance_path = 0;
+		for (int i = 0; i < path.size() - 1; i++) {
+			distance_path = distance_path + m_Distance[path.get(i)][path.get(i + 1)];
+		}
+		System.out.println("The new distance is " + distance_path);
+		return distance_path;
+	}
+	
+	public int caluculate_path_profit(List<Integer> path) {
+		int profit_path = 0;
+		for (int i = 0; i < path.size() ; i++) {
+			profit_path = profit_path + points_profit[path.get(i)] ;
+		}
+		System.out.println("The new profit is " + profit_path);
+		return profit_path;
+	}
+
+// next step is the local search
+
 
 }
